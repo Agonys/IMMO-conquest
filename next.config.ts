1e8d313bbc968e2553c874af7c0bbbc995dee719
Agonys <toxic325@gmail.com>
2025-06-19 01:44:08 +0200

    disable nextjs image optimization for cloudflare caching

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,22 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pino', 'pino-pretty'],
   images: {
     remotePatterns: [new URL('https://idlemmo-conquest.com/**')],
+    unoptimized: true,
   },
   allowedDevOrigins: ['192.168.0.87'],
+  async headers() {
+    return [
+      {
+        source: '/uploaded/:all*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=21600',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
