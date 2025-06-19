f493d5f297e79574e314f649fbe611425cba3fc2
Agonys <toxic325@gmail.com>
2025-06-19 02:33:25 +0200

    final next.js config headers adjustment - for now :)

--- next.config.ts
+++ next.config.ts
@@ -36,14 +36,6 @@ const nextConfig: NextConfig = {
             value: 'geolocation=(), microphone=(), camera=()',
           },
           {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
           },
